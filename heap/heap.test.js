15138876a44f16fa1c7e323847c1b4ebf7aff4a1
Mohammed Mamun <momamun@yahoo.com>
2020-05-30 23:43:06 -0400

    min and max

--- heap/heap.test.js
+++ heap/heap.test.js
@@ -1,11 +1,54 @@
-const Heap = require("./heap")
+const MaxBinaryHeap = require("./maxbinaryheap")
+const MinBinaryHeap = require("./minbinaryheap")
 
-test("Inserts Max Heap correctly", () => {
-    const heap = new Heap() 
+test("Add Max Heap correctly", () => {
+    const heap = new MaxBinaryHeap() 
     heap.add(2)
     heap.add(1)
     heap.add(3)
     heap.add(4)
-    console.log(heap)
     expect(heap).toEqual({values: [ 4, 3, 2, 1 ]})
+})
+
+/*
+ Min Binary Heap Test
+*/
+
+test("Add Min Heap correctly", () => {
+    const heap = new MinBinaryHeap() 
+    heap.add(2)
+    heap.add(1)
+    heap.add(3)
+    heap.add(4)
+    expect(heap).toEqual({ values: [ 1, 2, 3, 4 ] })
+})
+
+test("Return smallest element (root)", () => {
+    const heap = new MinBinaryHeap() 
+    heap.add(2)
+    heap.add(1)
+    heap.add(3)
+    heap.add(4)
+    expect(heap.getMin()).toEqual(1)
+})
+
+test("Removes minimum element from MinHeap", () => {
+    const heap = new MinBinaryHeap() 
+    heap.add(2)
+    heap.add(1)
+    heap.add(3)
+    heap.add(4)
+    const min = heap.extractMin()
+    expect(min).toEqual(1)
+})
+
+// touch uncovered line 46-48
+test("Removes minimum element from MinHeap", () => {
+    const heap = new MinBinaryHeap() 
+    heap.add(3)
+    heap.add(1)
+    heap.add(2)
+    heap.add(4)
+    const min = heap.extractMin()
+    expect(min).toEqual(1)
 })
