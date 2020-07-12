3fc1a5d41342675f5e25524e5bed39370828f838
Mohammed Mamun <momamun@yahoo.com>
2020-07-12 12:06:08 -0400

    impl stack

---
+++ stack/stack.js
@@ -0,0 +1,30 @@
+class Stack {
+    constructor() {
+        this.storage = {}
+        this.size = 0
+    }
+
+    push(element) {
+        this.size++
+        this.storage[this.size] = element
+    }
+
+    pop() {
+        let removed = this.storage[this.size]
+        delete this.storage[this.size]
+        return removed
+    }
+
+    peek() {
+        return this.storage[this.size]
+    }
+}
+
+const stack = new Stack();
+
+stack.push("dog")
+stack.push("cat")
+stack.push("mouse")
+
+console.log(stack.pop())
+console.log(stack)
