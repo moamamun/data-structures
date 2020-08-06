c3f07186fd27779579232d1ed1b75dded3536279
Mohammed Mamun <momamun@yahoo.com>
2020-08-06 18:14:12 -0400

    no duplicates

--- hash/hash.js
+++ hash/hash.js
@@ -15,11 +15,17 @@ class HashTable {
     return total
   }
   set(key, value) {
-    let index = this.hash(key)
-    if (!this.keyMap[index]) {
-      this.keyMap[index] = []
+    let index = this.hash(key);
+    if(!this.keyMap[index]) {
+        this.keyMap[index] = []
+    } else {
+        for (let i = 0; i < this.keyMap[index].length; i++) {
+            if(this.keyMap[index][i][0] === key) {
+                return
+            }
+        }
     }
-    this.keyMap[index].push([key, value])
+    this.keyMap[index].push([key,value])
   }
   get(key) {
     let index = this.hash(key)
